94e0ffde672d45cff8537bc98dd04b1933380377
glass <silver20190204@gmail.com>
2024-10-11 20:56:45 +0900

    Add read test file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,20 @@
 package monitoring.server
 
+import scala.io.Source
+import scala.util.{Success, Failure, Using}
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val filePath = "./a.txt"
+//    val filePath = "/proc/stat"
+    val result = Using(Source.fromFile(filePath)) {
+      source => source.getLines().toList
+//      _.getLines().toList
+    }
 
+    result match {
+      case Success(lines) => lines.foreach(println)
+      case Failure(exception) => println(s"Error message: ${exception.getMessage}")
+    }
   }
 }
