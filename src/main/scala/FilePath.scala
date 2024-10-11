74b5d23290a9485551fe9051323a862f8c56cc36
glass <silver20190204@gmail.com>
2024-10-11 22:02:48 +0900

    Add FilePath docs

--- src/main/scala/FilePath.scala
+++ src/main/scala/FilePath.scala
@@ -1,14 +1,31 @@
 package monitoring.server
 
+/**
+ * 모니터링하기 위한 리눅스 파일 경로들을 모아둔 추상 타입
+ */
 sealed trait FilePath {
+  /**
+   * 파일 경로 반환 메서드
+   * 
+   * @return 모니터링 대상인 파일의 경로
+   */
   def path: String
 }
 
+/**
+ * 실제 파일 경로들을 관리하는 객체
+ */
 object FilePath {
+  /**
+   * 테스트용 파일 경로
+   */
   case object Test extends FilePath {
     val path: String = "./a.txt"
   }
 
+  /**
+   * 리눅스의 cpu 사용량 모니터링 파일 경로
+   */
   case object ProcStat extends FilePath {
     val path: String = "/proc/stat"
   }
