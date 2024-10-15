eff3c37b98f9ad3a1ff8c6b6e60f6dfb9b04d3c8
glass <silver20190204@gmail.com>
2024-10-15 21:21:56 +0900

    Add FileReader

---
+++ src/main/scala/FileReader.scala
@@ -0,0 +1,25 @@
+package monitoring.server
+
+import scala.io.Source
+import scala.util.{Failure, Success, Using}
+
+/**
+ * 직접 파일에 접근해서 내용을 읽는 객체
+ */
+object FileReader {
+
+  /**
+   * 특정 파일을 읽고 내용을 반환하는 메서드
+   *
+   * @param path 읽기 대상 파일의 경로
+   * @return 파일을 정상적으로 읽었다면 파일 내용을 반환, 예외가 발생했다면 빈 내용을 반환
+   */
+  def read(path: String): List[String] = Using(Source.fromFile(path)) {
+    _.getLines().toList
+  } match {
+    case Success(lines) => lines
+    case Failure(exception) =>
+      println(s"Error message: ${exception.getMessage}")
+      List()
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,10 @@
 package monitoring.server
 
-import scala.io.Source
-import scala.util.{Success, Failure, Using}
-
 import FilePath._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val filePath = ProcStat.toString
-    val result = Using(Source.fromFile(filePath)) {
-      source => source.getLines().toList
-    }
-
-    result match {
-      case Success(lines) => lines.foreach(println)
-      case Failure(exception) => println(s"Error message: ${exception.getMessage}")
-    }
+    FileReader.read(ProcStat.toString)
+              .foreach(println)
   }
 }
